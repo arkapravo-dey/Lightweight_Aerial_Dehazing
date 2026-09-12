f3208cf5193cd60168cf982e5aceb80941e7151e
Arkapravo Dey <arka2005dey@gmail.com>
2026-09-13 03:07:34 +0530

    Code Updated

--- models/network.py
+++ models/network.py
@@ -54,14 +54,6 @@ class LayerNorm2d(nn.Module):
             self.eps
         )
 
-
-class SimpleGate(nn.Module):
-
-    def forward(self, x):
-        x1, x2 = x.chunk(2, dim=1)
-        return x1 * x2
-
-
 class MSPA(nn.Module):
     def __init__(self, channels, N=3):
         super().__init__()
